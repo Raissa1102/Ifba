974bd635c4aee58bb75f88aaa80a1719089c8440
Raissa <dev.raissaaraujo@gmail.com>
2026-02-21 08:54:53 -0300

    .

---
+++ carros.py
@@ -0,0 +1,4 @@
+from veiculo import Veiculo
+class Carro (Veiculo):
+    def dirigir():
+        return "Este carro está em movimento"
---
+++ veiculo.py
@@ -0,0 +1,5 @@
+#Criar a classe veiculo
+class Veiculo():
+    #Escrever o método dirigir
+    def dirigir():
+        return "O veiculo está em movimento"
